d5cb88eedec05b0827f39e1a43b8f325e059be71
ranuthd <ranuthofficial@gmail.com>
2024-08-24 17:33:15 +0530

    Finished working on project service

--- src/com/model/Project.java
+++ src/com/model/Project.java
@@ -4,24 +4,24 @@ public class Project
 {
     private int[] projectPerformance = new int[3];
 
-    public Project ( int performance1, int performance2, int performance3)
+    public Project ( int score1, int score2, int score3)
     {
-        this.projectPerformance[0] = performance1;
-        this.projectPerformance[1] = performance2;
-        this.projectPerformance[2] = performance3;
+        this.projectPerformance[0] = score1;
+        this.projectPerformance[1] = score2;
+        this.projectPerformance[2] = score3;
     }
 
-    public int getperformance1()
+    public int getscore1()
     {
         return projectPerformance[0];
     }
 
-    public int getperformance2()
+    public int getscore2()
     {
         return projectPerformance[1];
     }
 
-    public int getperformance3()
+    public int getscore3()
     {
         return projectPerformance[2];
     }
--- src/com/service/ProjectService.java
+++ src/com/service/ProjectService.java
@@ -2,6 +2,34 @@ package com.service;
 
 public class ProjectService
 {
+    public double calculateAverageScore( int score1, int score2, int score3 )
+    {
+        int sum = score1 + score2 + score3;
+        return ( ( double ) sum / 3 );
+    }
 
+
+    public String calculateGradeScore( int score1, int score2, int score3 )
+    {
+        double average = calculateAverageScore( score1, score2, score3 );
+        int roundedAverage = ( int ) Math.round( average );
+        if( average >= 80 )
+        {
+            return "Outstanding! -> " + roundedAverage;
+        }
+        else if( average >= 70 )
+        {
+            return "Exceeds Expectations! -> " + roundedAverage;
+        }
+        else if( average >= 40 )
+        {
+            return "Meets Expectations! -> " + roundedAverage;
+        }
+        else
+        {
+            return "Needs Improvement! -> " + roundedAverage;
+        }
+    }
 }
+
 
