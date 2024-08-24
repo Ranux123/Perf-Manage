d9bec472701b0d776026db098d1c3e279c878e5b
ranuthd <ranuthofficial@gmail.com>
2024-08-24 15:41:18 +0530

    Finished working on the constructors

--- src/com/model/Employee.java
+++ src/com/model/Employee.java
@@ -2,4 +2,33 @@ package com.model;
 
 public class Employee
 {
+    private String employeeName;
+    private int employeeId;
+    private Project project;
+
+    public Employee(String employeeName, int employeeId )
+    {
+        this.employeeName = employeeName;
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName()
+    {
+        return employeeName;
+    }
+
+    public String setEmployeeName(String employeeName)
+    {
+        return this.employeeName = employeeName;
+    }
+
+    public int getEmployeeId()
+    {
+        return employeeId;
+    }
+
+    public int setEmployeeId( int employeeId)
+    {
+        return this.employeeId = employeeId;
+    }
 }
--- src/com/model/Project.java
+++ src/com/model/Project.java
@@ -2,4 +2,27 @@ package com.model;
 
 public class Project
 {
+    private int[] projectPerformance = new int[3];
+
+    public Project ( int performance1, int performance2, int performance3)
+    {
+        this.projectPerformance[0] = performance1;
+        this.projectPerformance[1] = performance2;
+        this.projectPerformance[2] = performance3;
+    }
+
+    public int getperformance1()
+    {
+        return projectPerformance[0];
+    }
+
+    public int getperformance2()
+    {
+        return projectPerformance[1];
+    }
+
+    public int getperformance3()
+    {
+        return projectPerformance[2];
+    }
 }
